f38c2761a516e03e8add583b1128d78460e12cbc
sanjaraT <sanjaratunola@gmail.com>
2026-01-17 12:39:28 +0600

    Linear Regression

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
 
 df  = pd.read_csv("Housing.csv")
 # print(df.head())
 
-print(df.isnull().sum())
-print(df.shape)
-print(df.duplicated().sum())
-print(df.dtypes)
+# print(df.isnull().sum())
+# print(df.shape)
+# print(df.duplicated().sum())
+# print(df.dtypes)
 
 #Boxplot
 num_cols = df.select_dtypes(include=["int64"]).columns
@@ -20,13 +22,13 @@ for col in num_cols:
     plt.figure(figsize=(5,3))
     sns.boxplot(x=df[col])
     plt.title(f"Outliers in {col}")
-    plt.show()
+    # plt.show()
 
 binary_cols =["mainroad","guestroom","basement","hotwaterheating","airconditioning",
               "prefarea"]
 df[binary_cols] = df[binary_cols].replace({"yes":1,"no":0})
 df = pd.get_dummies(df, columns=["furnishingstatus"],drop_first=True)
-print(df.head())
+# print(df.head())
 
 target = 'price'
 
@@ -38,17 +40,18 @@ for col in df.columns:
         plt.xlabel(col)
         plt.ylabel(target)
         plt.title(f"{col}vs{target}")
-        plt.show()
+        # plt.show()
 
 #Correlation before 
 corr_matrix_b = df.corr()
 plt.figure(figsize=(12,10))
 sns.heatmap(corr_matrix_b, annot=True, fmt=".2f",cmap="coolwarm")
 plt.title("Correlation Matrix Before Preprocessing")
-plt.show()
+# plt.show()
+df["price_log"] = np.log1p(df["price"])
 
-X = df.drop(columns=['price'])
-y = df['price']
+X = df.drop(columns=['price_log'])
+y = df['price_log']
 
 #Split
 X_train, X_temp, y_train, y_temp = train_test_split(X,y, test_size=0.30,random_state=42)
@@ -61,6 +64,25 @@ X_val_scaled = scaler.transform(X_val)
 x_test_scaled = scaler.transform(X_test)
 
 df_scaled = pd.DataFrame(X_train_scaled, columns= X_train.columns)
-print(df_scaled.head())
+# print(df_scaled.head())
+
+#Evaluation Matrix
+def evaluate(model, X_tr, y_tr, X_val, y_val):
+    model.fit(X_tr,y_tr)
+    pred = model.predict(X_val)
+    rmse = np.sqrt(mean_squared_error(y_val, pred))
+    r2 = r2_score(y_val, pred)
+    return rmse, r2
+
+#Linear Regression
+lr = LinearRegression()
+rmse_lr, r2_lr = evaluate(
+    lr, X_train_scaled, y_train, X_val_scaled, y_val
+)
+print("Linear Regreession")
+print("RMSE: ", rmse_lr)
+print("R2: ", r2_lr)
+
+
 
 
